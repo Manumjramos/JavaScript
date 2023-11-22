a906775461716426f2b2ba97e8902565863b4c36
Manumjramos <144954024+Manumjramos@users.noreply.github.com>
2023-11-22 15:57:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+let nome
+
+console.log(typeof nome)
+
+let cidade
+
+console.log(typeof cidade)
+
+nome = prompt('Digite seu nome')
+cidade = prompt("Digite a cidade que você mora")
+
+console.log('Olá, meu nome é ${nome} e moro em ${cidade}')
+
+
+let primeiraPergunta = prompt("Você gosta de açaí?") 
+console.log("Você gosta de açaí?", primeiraPergunta)
+
+
+let segundaPergunta = prompt("Você gosta de praia?") 
+console.log("Você gosta de praia?", segundaPergunta)
+
+let x = 5
+let y = 10
+
+[x,y]=[y,x]
+
+
+
+
+
+
